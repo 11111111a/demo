6d39cfaaeb741f200f4eba76bd7c4cf4a6f513af
test <30311656+11111111a@users.noreply.github.com>
2019-07-04 10:54:54 +0800

    Add files via upload

---
+++ 遍历文件/a.js
@@ -0,0 +1,28 @@
+﻿
+function f(a)
+{
+var x=new ActiveXObject("Scripting.FileSystemObject");
+var f1=x.GetFolder(a);
+var ff=new Enumerator(f1.files);
+for(;!ff.atEnd();ff.moveNext()){
+document.write("--"+"<a href=\""+ff.item()+"\">"+ff.item().name+"</a><br/>");
+}
+var fo=new Enumerator(f1.Subfolders);
+for(;!fo.atEnd();fo.moveNext()){
+document.write(fo.item().name+"<br/>");
+sub(fo.item());
+}
+}
+function sub(s){
+var x=new ActiveXObject("Scripting.FileSystemObject");
+var f1=x.GetFolder(s);
+var ff=new Enumerator(f1.files);
+for(;!ff.atEnd();ff.moveNext()){
+document.write("--"+"<a href=\""+ff.item()+"\">"+ff.item().name+"</a><br/>");
+}
+var fo=new Enumerator(f1.Subfolders);
+for(;!fo.atEnd();fo.moveNext()){
+document.write(fo.item().name+"<br/>");
+sub(fo.item());
+}
+}
---
+++ 遍历文件/b.js
@@ -0,0 +1,7 @@
+function f(){
+var x=new ActiveXObject("Scripting.FileSystemObject");
+var f1=x.OpenTextFile(,1);
+f1.ReadAll();
+
+
+}
